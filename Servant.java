482a573f3bc25312287a47ef518f9f3eb2f4d357
vuhger <victor.uhnger@gmail.com>
2025-09-05 16:25:08 +0200

    Added precode, test files and assignment.

--- Servant.java
+++
@@ -1,6 +0,0 @@
-interface Servant{
- int getPopulationofCountry(String countryName);
- int getNumberofCities(String countryName, int threshold, int comparison);
- int getNumberofCountries(int cityCount, int threshold, int comp);
- int getNumberofCountriesMM(int cityCount, int minPopulation, int maxPopulation);
-}
