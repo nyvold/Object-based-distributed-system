fbfc150398683bbc27d81321414c99c1b7563d49
vuhger <victor.uhnger@gmail.com>
2025-09-08 09:49:02 +0200

    format Latency.java

--- test-pre/solution/src/main/java/com/ass1/server/Latency.java
+++ test-pre/solution/src/main/java/com/ass1/server/Latency.java
@@ -2,7 +2,7 @@ package com.ass1.server;
 
 public final class Latency {
     public static long commMs(int clientZone, int serverZone, int ringSize) {
-        int d = (serverZone >= clientZone) ? (serverZone - clientZone)                                          : (ringSize - (clientZone - serverZone));
+        int d = (serverZone >= clientZone) ? (serverZone - clientZone) : (ringSize - (clientZone - serverZone));
         return 80L + d * 30L; // same-zone = 80ms
     }
 }
