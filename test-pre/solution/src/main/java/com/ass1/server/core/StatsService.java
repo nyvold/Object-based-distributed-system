4fcee9cbeb9f9c3029f3deb552ff5bdfdcd09ad1
vuhger <victor.uhnger@gmail.com>
2025-09-13 10:23:31 +0200

    add StatsService (server should call this

--- test-pre/solution/src/main/java/com/ass1/server/core/StatsService.java
+++ test-pre/solution/src/main/java/com/ass1/server/core/StatsService.java
@@ -1,5 +1,32 @@
+// StatsService.java
 package com.ass1.server.core;
 
-public class StatsService {
-    
+import com.ass1.server.db.CityRepository;
+
+public final class StatsService {
+    private final CityRepository cityRepo;
+
+    public StatsService(CityRepository cityRepo) {
+        this.cityRepo = cityRepo;
+    }
+
+    // a) getPopulationofCountry(countryName)
+    public long getPopulationofCountry(String countryName) throws Exception {
+        return cityRepo.sumPopulationByCountryName(countryName);
+    }
+
+    // b) getNumberofCities(countryName, threshold, comp)
+    public long getNumberofCities(String countryName, long threshold, String comp) throws Exception {
+        return cityRepo.countCitiesByCountryThreshold(countryName, threshold, comp);
+    }
+
+    // c) getNumberofCountries(citycount, threshold, comp)
+    public long getNumberofCountries(long citycount, long threshold, String comp) throws Exception {
+        return cityRepo.countCountriesWithCitycountAndThreshold(citycount, threshold, comp);
+    }
+
+    // d) getNumberofCountriesMM(citycount, minpopulation, maxpopulation)
+    public long getNumberofCountriesMM(long citycount, long minPop, long maxPop) throws Exception {
+        return cityRepo.countCountriesWithCitycountBetween(citycount, minPop, maxPop);
+    }
 }
