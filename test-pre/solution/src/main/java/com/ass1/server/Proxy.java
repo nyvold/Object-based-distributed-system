9428f3ee6a1e9de42fb976c677bf300686a15de4
Jonas Berger Nyvold <jonas.nyvold02@gmail.com>
2025-09-18 18:46:05 +0200

    updated LoadBalancer algorithm with correct rule order

--- test-pre/solution/src/main/java/com/ass1/server/Proxy.java
+++ test-pre/solution/src/main/java/com/ass1/server/Proxy.java
@@ -12,39 +12,6 @@ public class Proxy implements ProxyInterface {
     // Proxy composition: Proxy, Refresher, LoadBalancer
     // Proxy responsability: entry point for client requests, registration of servers, and coordination
 
-    // our proxy must:
-    // - distribute requests based on clients zone
-    // - have a overview of each servers workload, this overview is updated every 18
-    // requests?
-    // - request delegation happends in independent thread
-    // - workload updating happends in separate thread
-    // - assign zone to servers in ascending order, and record servers IP adress and
-    // port
-    // - simulate connection time between client and server
-
-    // rules:
-    // step 1: if server overloaded (18>=requests)
-    // try server with least requests in waiting list
-    // step 2: if several servers have same amount of requests
-    // server closest to the client (clockwise) is chosen
-    // step 3: if all servers overloaded
-    // server in same zone is chosen
-    // step 4: if zone has no server:
-    // client is redirected to closest one (clockwise) and repeats steps 1-3
-
-    // functions:
-    // 1. get server via registry (zone in query)
-    // 2. queue check if server has capasity
-    // 3. parse input
-    // 4. register servers (via API?)
-    // 5. calculate client-server connection delay
-
-    // proxy gets request from client, and returns a server that can handle the
-    // request
-    // public Proxy(){
-    // Registry registry = LocateRegistry.getRegistry();
-    // }
-
     private final Registry registry;
     private final LoadBalancer balancer;
     private final Refresher refresher;
